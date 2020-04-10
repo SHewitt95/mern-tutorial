9bd2ba5e09aa5a88bd819aae674c7c0df6978f63
SHewitt95 <hewitt.sherman@gmail.com>
2020-04-09 20:28:51 -0400

    MERN: part 1

---
+++ server.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const items = require('./routes/api/items');
+
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = process.env['DB_URL_DEV'];
+
+// Connect to MongoDB
+mongoose.connect(db)
+  .then(() => console.log('MONGO IS CONNECTED'))
+  .catch(err => console.error(err));
+
+app.use('/api/items', items);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
