3cea8130390b024992cfd54c7f8643cd7710dff6
SHewitt95 <hewitt.sherman@gmail.com>
2020-04-11 13:30:54 -0400

    add simple list ui

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+const List = ({ items }) => {
+  return (
+    <ul>
+      {items.map((item, idx) => <li key={idx}>{item}</li>)}
+    </ul>
+  );
+};
+
 function App() {
+  const [items, addItem] = useState([]);
+  const [value, setValue] = useState('');
   return (
     <div className="App">
       <p>Hello</p>
+      <List items={items} />
+      <form onSubmit={e => {
+        e.preventDefault();
+        addItem([...items, value])
+      }}>
+        <input value={value} onChange={e => setValue(e.target.value)} />
+        <button type="submit">Add Item</button>
+      </form>
     </div>
   );
 }
