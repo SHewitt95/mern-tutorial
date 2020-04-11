63d169a424ff539be669ad6654fda7321ff1dca7
SHewitt95 <hewitt.sherman@gmail.com>
2020-04-11 15:17:44 -0400

    query items from DB

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 const List = ({ items }) => {
@@ -9,16 +10,35 @@ const List = ({ items }) => {
   );
 };
 
+const useItems = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('/api/items')
+      .then(res => {
+        console.log({res})
+        if (res.status === 200 && res.statusText === 'OK') {
+          const resItems = res.data.map(item => item.name);
+          setItems(resItems);
+        }
+      })
+  }, []);
+
+  return items;
+}
+
 function App() {
-  const [items, addItem] = useState([]);
+  const items = useItems();
   const [value, setValue] = useState('');
+
   return (
     <div className="App">
       <p>Hello</p>
       <List items={items} />
       <form onSubmit={e => {
         e.preventDefault();
-        addItem([...items, value])
+        // addItem([...items, value])
       }}>
         <input value={value} onChange={e => setValue(e.target.value)} />
         <button type="submit">Add Item</button>
